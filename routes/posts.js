a18f9cde98819e64f503caa3fa39a89d8ec47b68
PawleN-D <sirpawle@gmail.com>
2020-04-24 04:31:37 +0200

    added the posts routes

---
+++ routes/posts.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID;
+
+const ensureAunthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+};
+
+router.get('/create-post', (req, res, next) => {
+    const flash = req.flash();
+    const error = flash.error || [];
+    const success = flash.success || [];
+  
+    res.render('create-post', { error, success });
+});
+
+router.post('/create-post', (req, res, next) => {
+    const { title, content } = req.body;
+    const username = req.user.username;
+    const posts = req.app.locals.posts;
+    const date = new Date().toISOString();
+      posts
+      .insertOne({ tilte, content, date, author: username })
+      .then(() => {
+        req.flash('success', 'Post created successfully');
+        res.redirect('/create-post');
+      })
+      .catch(() => {
+        req.flash('error', 'Your blog post could not be created!');
+        res.redirect('/create-post');
+      });
+});
+
+router.get('/post', (req, res, next) => {
+    const posts = req.app.locals.posts;
+
+    posts
+        .find({})
+        .toArray()
+        .then(posts => res.render('posts', { posts }));
+});
+
+router.get('/posts/:id', (req, res, next) => {
+    const posts = req.app.locals.posts;
+    const postID = ObjectID(req.params.id);
+
+    posts.find({ _id: postID }).then(post => res.render('post', { post }));
+});
+
+module.exports = router;
