c6b6aff4c189be7209c2b3d5fe6e6a55e9f0430b
PawleN-D <sirpawle@gmail.com>
2020-04-24 03:50:05 +0200

    inserted the login and register routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', (req, res, next) => {
+  const errors = req.flash().error || [];
+  res.render('login', { errors });
+});
+
+router.post('/login', passport.authenticate('local', {
+  failureFlash: true,
+  failureRedirect: '/login',
+}), (req, res, next) => {
+  res.redirect('/create-post');
 });
 
 module.exports = router;
