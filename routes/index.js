8b2fb0a0aa78c73fbc47a29a3951a41d9433ccc2
PawleN-D <sirpawle@gmail.com>
2020-04-25 01:49:03 +0200

    working on the home page to display blog post

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/', (req, res, next) => {
+  const posts = req.app.locals.posts;
+
+  posts
+      .find({})
+      .toArray()
+      .then(posts => res.render('posts', { posts }));
 });
+
 
 module.exports = router;
