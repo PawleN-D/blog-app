cd7d69199dd7b612ab8635fd0c807edf94d6e967
PawleN-D <sirpawle@gmail.com>
2020-04-26 15:12:35 +0200

    fixed the view bug stopping some views to show

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,18 @@ const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-router.get('/', (req, res, next) => {
-  const posts = req.app.locals.posts;
-
-  posts
-      .find({})
-      .toArray()
-      .then(posts => res.render('posts', { posts }));
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
+
+// router.get('/', (req, res, next) => {
+//   const posts = req.app.locals.posts;
+
+//   posts
+//       .find({})
+//       .toArray()
+//       .then(posts => res.render('posts', { posts }));
+// });
 
 
 module.exports = router;
